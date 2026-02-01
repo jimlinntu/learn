eaa75363bfa54579027112417436d5e23ae82f4a
Jim Lin <tzuchuan.lin7@gmail.com>
2026-02-01 14:41:45 +0800

    Saves progress of implementing ragged_all_to_all moe

--- jax/moe_try_test.py
+++ jax/moe_try_test.py
@@ -16,18 +16,32 @@ class TestMoe(unittest.TestCase):
     def setUp(self):
         super().setUp()
         self.assertEqual(jax.device_count(), 12)
-        self.mesh = jax.sharding.Mesh(np.array(jax.devices()).reshape(3, 4),
-                                      axis_names=("x", "y"))
+        self.mesh = jax.sharding.Mesh(
+            np.array(jax.devices()).reshape(3, 4),
+            axis_names=("x", "y"),
+            axis_types=(jax.sharding.AxisType.Explicit,
+                        jax.sharding.AxisType.Explicit))
+
+        self.device_count = jax.device_count()
+        self.x, self.y = 3, 4
+        self.num_data_parallelism = self.x * self.y
+        self.num_expert_parallelism = self.y  # Expert params is sharded `y` ways
 
         self.B = 36  # Batch size
         self.T = 128  # Number of tokens per example
         self.H = 256  # Hidden size
-        self.E = 48  # The numbder of experts
+        self.E = 48  # The number of experts
         self.K = 3  # Top-k experts to route to
+        # Expert's capacity:
+        # If tokens are distributed perfectly equal, each device will get:
+        # (BTK // num_expert_parallelim) tokens. Adding a 2 to leave some headroom
+        # on each device.
+        self.EC = int(
+            (self.B * self.T * self.K) // self.num_expert_parallelism * 2)
 
         self.input_sharding = jax.NamedSharding(self.mesh, spec=P(("x", "y")))
         self.replicated_sharding = jax.NamedSharding(self.mesh, spec=P())
-        self.expert_sharding = jax.NamedSharding(self.mesh, spec=P(("x", "y")))
+        self.expert_sharding = jax.NamedSharding(self.mesh, spec=P("y"))
 
         # (B_xy, T, H)
         self.inputs: jax.Array = jax.device_put(
@@ -52,7 +66,8 @@ class TestMoe(unittest.TestCase):
 
     def _create_identity_experts(self) -> jax.Array:
         """Creates identity expert parameters using jax.vmap"""
-        experts = jax.vmap(lambda _: jnp.identity(n=self.H))(jnp.arange(self.E))
+        experts = jax.vmap(lambda _: jnp.identity(n=self.H))(jnp.arange(
+            self.E))
         self.assertEqual(experts.shape, (self.E, self.H, self.H))
         return experts
 
@@ -60,7 +75,7 @@ class TestMoe(unittest.TestCase):
         """Ensures if experts are all initialized as I, x == moe(x) holds"""
 
         result = jax.jit(
-            functools.partial(_naive_moe, topk=3),
+            functools.partial(_naive_moe, topk=3, mesh=self.mesh),
             in_shardings=(self.input_sharding, self.replicated_sharding,
                           self.expert_sharding))(self.inputs,
                                                  self.router_param,
@@ -68,12 +83,78 @@ class TestMoe(unittest.TestCase):
         self.assertEqual(result.shape, self.inputs.shape)
         np.testing.assert_allclose(result, self.inputs, rtol=1e-6)
 
+    def test_ragged_all_to_all_moe(self):
+        result = jax.jit(
+            functools.partial(
+                _ragged_all_to_all_moe,
+                topk=3,
+                num_data_parallelism=self.num_data_parallelism,
+                num_expert_parallelism=self.num_expert_parallelism,
+                mesh=self.mesh),
+            in_shardings=(self.input_sharding, self.replicated_sharding,
+                          self.expert_sharding))(self.inputs,
+                                                 self.router_param,
+                                                 self.experts)
+
+    def test_compute_ragged_all_to_all_info(self):
+        num_tokens_per_device = 13
+
+        # Creates fake tokens to test distributing these
+        # tokens to devices and bringing them back
+        fake_tokens = jax.vmap(lambda _: jnp.arange(0, num_tokens_per_device))(jnp.arange(0, self.device_count)).reshape(
+            num_tokens_per_device * self.device_count
+        )
+
+        fake_expert_indices = jax.random.randint(jax.random.key(1234),
+                                                 shape=(num_tokens_per_device *
+                                                        self.device_count, ),
+                                                 minval=0,
+                                                 maxval=self.E)
+
+        fake_expert_indices = jax.device_put(fake_expert_indices,
+                                             self.input_sharding)
+        fake_tokens = jax.device_put(fake_tokens, self.input_sharding)
+        
+        @jax.shard_map(
+            in_specs=(self.input_sharding.spec, self.input_sharding.spec),
+            out_specs=(),
+            mesh=self.mesh,
+            check_vma=False)
+        def _func(local_tokens, local_expert_indices):
+            sorter = jnp.argsort(local_expert_indices)
+
+            sorted_local_tokens = local_tokens[sorter]
+            sorted_expert_indices = local_expert_indices[sorter]
+
+            device_expert_start_indices = _get_device_expert_start_indices(self.E, self.num_expert_parallelism)
+
+            input_offsets = jnp.searchsorted(sorted_expert_indices,
+                                            device_expert_start_indices)
+            # jax.debug.print("sorted tokens {}, sorted indices: {}, input_offsets: {}", sorted_local_tokens, sorted_expert_indices, input_offsets)
+
+
+            num_tokens = sorted_expert_indices.shape[0]
+            send_sizes, recv_sizes, output_offsets = _compute_ragged_all_to_all_info(num_tokens, input_offsets)
+            xidx, yidx = jax.lax.axis_index("x"), jax.lax.axis_index("y")
+            jax.debug.print("(x, y) = ({}, {}), sorted_expert_indices: {}, input_offsets: {}, send_sizes: {}, recv_sizes: {}, output_offsets: {}",
+                            xidx, yidx, sorted_expert_indices, input_offsets, send_sizes, recv_sizes, output_offsets)
+
+        
+        _func(fake_tokens, fake_expert_indices)
+        raise ValueError(fake_expert_indices.addressable_shards[1])
+
 
 def _naive_moe(inputs: jax.Array, router_param: jax.Array, experts: jax.Array,
-               topk: int) -> jax.Array:
-    B, T, H = inputs.shape
-    H, E = router_param.shape
-    E, H, H = experts.shape
+               topk: int, mesh: jax.sharding.Mesh) -> jax.Array:
+    B, T, H1 = inputs.shape
+    H2, E1 = router_param.shape
+    E2, H3, H4 = experts.shape
+
+    assert H1 == H2 == H3 == H4
+    assert E1 == E2
+
+    H = H1
+    E = E1
 
     # (B, T, E)
     routing_weights = jnp.einsum("BTH,HE->BTE", inputs, router_param)
@@ -90,7 +171,10 @@ def _naive_moe(inputs: jax.Array, router_param: jax.Array, experts: jax.Array,
     # NOTE: In practice, we don't want to make lots of copies of moe weights
     #       because the amount of weights can be huge.
     # (B, T, K) takes (E, H, H) -> (B, T, K, H, H)
-    expert_param_each_token = jnp.take(experts, indices=expert_indices, axis=0)
+    all_gathered_experts = jax.reshard(experts,
+                                       jax.sharding.NamedSharding(mesh, P()))
+    expert_param_each_token = all_gathered_experts.at[expert_indices].get(
+        out_sharding=jax.sharding.NamedSharding(mesh, P(("x", "y"))))
 
     # (B, T, K, H, H) @ (B, T, H) = (B, T, K, H)
     #           ^              ^ <--- contraction
@@ -105,6 +189,129 @@ def _naive_moe(inputs: jax.Array, router_param: jax.Array, experts: jax.Array,
     assert result.shape == (B, T, topk, H)
     # (B, T, K, H) -> (B, T, H)
     return jnp.sum(result, axis=2)
+
+
+def _ragged_all_to_all_moe(
+        inputs: jax.Array,
+        router_param: jax.Array,
+        experts: jax.Array,
+        *,
+        # Other Jax-untrace-able variables
+        topk: int,
+        num_data_parallelism: int,
+        num_expert_parallelism: int,
+        mesh: jax.sharding.Mesh):
+    """Implements tokens distribution and collection by AllToAll collective."""
+    B, T, H1 = inputs.shape
+    H2, E1 = router_param.shape
+    E2, H3, H4 = experts.shape
+
+    assert H1 == H2 == H3 == H4
+    assert E1 == E2
+
+    H = H1
+    E = E1
+
+    # (B, T, E)
+    routing_weights = jnp.einsum("BTH,HE->BTE", inputs, router_param)
+    assert routing_weights.shape == (B, T, E)
+
+    # (B, T, E) -> Expert indices (B, T, K) and top k weights (B, T, K)
+    expert_weights, expert_indices = jax.lax.top_k(routing_weights,
+                                                   k=topk,
+                                                   axis=-1)
+    expert_weights = jax.nn.softmax(expert_weights, axis=-1)
+    assert expert_indices.shape == (B, T, topk)
+
+    # Broadcasts (B, T, H) to (B, T, K, H) before distributing token
+    # vectors to each device
+    duplicate_inputs = jnp.broadcast_to(jnp.expand_dims(inputs, axis=2),
+                                        shape=(B, T, topk, H))
+    assert duplicate_inputs.shape == (B, T, topk, H)
+
+    @jax.shard_map(
+        in_specs=(
+            P(("x", "y")),
+            P(("x", "y")),
+            P("y"),  # Experts are sharde only along y axis
+        ),
+        out_specs=P(("x", "y")),
+        mesh=mesh,
+        check_vma=False,
+    )
+    def _shard_map_func(local_inputs: jax.Array,
+                        local_expert_indices: jax.Array,
+                        local_experts: jax.Array):
+        assert local_inputs.shape == (B // num_data_parallelism, T, topk, H)
+        assert local_expert_indices.shape == (B // num_data_parallelism, T,
+                                              topk)
+        assert local_experts.shape == (E // num_expert_parallelism, H, H)
+
+        # Flattens inputs and expert indices
+        flattened_inputs = jnp.reshape(local_inputs, shape=(-1, H))
+        flattened_expert_indices = jnp.reshape(local_expert_indices,
+                                               shape=(-1, ))
+
+        # Argsorts expert indices
+        sorter = jnp.argsort(flattened_expert_indices)
+
+        sorted_expert_indices = flattened_expert_indices[sorter]
+        sorted_flattened_inputs = flattened_inputs[flattened_expert_indices]
+        assert sorted_expert_indices.shape == (B * T * topk //
+                                               num_data_parallelism, )
+        assert sorted_flattened_inputs.shape == (B * T * topk //
+                                                 num_data_parallelism, H)
+
+        # Computes expert boundaries for `ragged_all_to_all`'s `input_offsets`
+        device_expert_start_indices = _get_device_expert_start_indices(
+            E, num_expert_parallelism)
+        assert device_expert_start_indices.shape == (jax.lax.axis_size("y"), )
+
+        input_offsets = jnp.searchsorted(sorted_expert_indices,
+                                         device_expert_start_indices)
+
+        return
+
+    # (B, T, K, H) -> (B, T, K, H) (transformed by experts)
+    tokens_after_experts = _shard_map_func(duplicate_inputs, expert_indices,
+                                           experts)
+
+
+def _get_device_expert_start_indices(num_experts: int,
+                                     num_expert_parallelism: int) -> jax.Array:
+    """Returns an 1D array with each device's starting expert index."""
+    assert num_experts % num_expert_parallelism == 0
+    num_experts_per_device = num_experts // num_expert_parallelism
+
+    # Let `num_experts_per_device=x`
+    # [0, E_x, E_2x, ..., E_num_experts-x]
+    return jnp.arange(start=0, stop=num_experts, step=num_experts_per_device)
+
+
+def _compute_ragged_all_to_all_info(num_tokens: int,
+                                    input_offsets: jax.Array):
+    """Computes info needed for `ragged_all_to_all` in `jax.shard_map`."""
+
+    send_sizes = jnp.concat([input_offsets[1:],
+                             jnp.array([num_tokens])]) - input_offsets
+    recv_sizes = jax.lax.all_to_all(
+        send_sizes,
+        # Ideally we can paramaterize this
+        axis_name="y",
+        split_axis=0,
+        concat_axis=0,
+        tiled=False,  # not need to tile because len(send_sizes) == |y|
+    )
+    send_to_me_offsets = jnp.concat([jnp.array([0]), jnp.cumsum(recv_sizes, axis=0)[:-1]])
+    me_to_dst_offsets = jax.lax.all_to_all(
+        send_to_me_offsets,
+        axis_name="y",
+        split_axis=0,
+        concat_axis=0,
+        tiled=False,  # not need to tile because len(send_sizes) == |y|
+    )
+    output_offsets = me_to_dst_offsets
+    return send_sizes, recv_sizes, output_offsets
 
 
 if __name__ == "__main__":
